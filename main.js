95a2dd6b440f380f2dc27d1b077c92aeaf27489b
Rhea Abayan <rhenayanz@gmail.com>
2021-03-26 18:30:22 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var elem = document.querySelector('.feature-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  wrapAround: true,
+  freeScroll: true,
+  prevNextButtons: false,
+  pageDots: false,
+ 
+});
+
+
+
+
