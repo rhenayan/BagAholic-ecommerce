faa08046f753a4271d27b9496248e8d35b726b6d
Rhea Abayan <rhenayanz@gmail.com>
2021-03-27 19:13:28 +0800

    updated files

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-var elem = document.querySelector('.feature-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true,
-  wrapAround: true,
-  freeScroll: true,
-  prevNextButtons: false,
-  pageDots: false,
- 
-});
+
+
+// FILTERING
+
+const category = document.querySelector('.category-tab');
+const filterProduct = document.querySelectorAll('.product-wrapper');
+
+// console.log(filterProduct)
+
+category.addEventListener('click', (selectedCategory) => {
+
+
+    if(selectedCategory.target.classList.contains('category')) {
+      const active = document.querySelector('.js-active');
+      active.classList.remove('js-active');
+
+     selectedCategory.target.classList.add('js-active');
+
+     
+      const filterCategory =  selectedCategory.target.getAttribute('data-name');
+      console.log(filterCategory)
+
+      filterProduct.forEach((product) => {
+        const filterItem = product.getAttribute('data-name');
+        console.log(filterItem)
+
+        if((filterItem == filterCategory) || filterCategory == 'all' ) {
+          product.classList.remove('hide');
+          product.classList.add('show');
+
+        } else {
+          product.classList.add('hide');
+          product.classList.remove('show');
+
+        }
 
 
 
+      });
+      
+      
+    }
+
+})
+    
 
